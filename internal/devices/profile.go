b8c4c92de890e5c91276c7946c81e2ba0e8391c5
Adam Charrett <adcharre@synamedia.com>
2022-12-12 17:04:49 +0000

    Switch to JSON encoded profiles

--- internal/devices/profile.go
+++ internal/devices/profile.go
@@ -1,7 +1,7 @@
 package devices
 
 import (
-	"github.com/fxamacker/cbor/v2"
+	"encoding/json"
 	"gopkg.in/yaml.v2"
 )
 
@@ -9,14 +9,13 @@ type ProfileEntry map[string]any
 type ProfileEntries []ProfileEntry
 
 type Profile struct {
-	_       struct{} `cbor:",toarray"`
-	Version int
-	Profile map[string]ProfileEntries
+	Version string                    `json:"version"`
+	Profile map[string]ProfileEntries `json:"components"`
 }
 
 func decodeProfile(payload []byte) (string, error) {
 	profile := Profile{}
-	if err := cbor.Unmarshal(payload, &profile); err == nil {
+	if err := json.Unmarshal(payload, &profile); err == nil {
 		if profileBytes, err := yaml.Marshal(&profile.Profile); err == nil {
 			return string(profileBytes), nil
 
@@ -30,12 +29,12 @@ func decodeProfile(payload []byte) (string, error) {
 
 func encodeProfile(profileStr string) ([]byte, error) {
 	profile := Profile{}
-	profile.Version = 2
+	profile.Version = "1.0"
 	profile.Profile = make(map[string]ProfileEntries)
 	if err := yaml.Unmarshal([]byte(profileStr), profile.Profile); err != nil {
 		return nil, err
 	}
-	bytes, err := cbor.Marshal(profile)
+	bytes, err := json.Marshal(profile)
 	if err != nil {
 		return nil, err
 	}
